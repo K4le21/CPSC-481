b4f13d80211c5e582274cd5f4b886eff40d33b54
rlu132 <98561497+rlu132@users.noreply.github.com>
2024-12-12 00:03:22 -0800

    final ver

--- neuralnet.py
+++ neuralnet.py
@@ -1,46 +1,63 @@
 import numpy as np
 import random
 
-
 class NeuralNetwork():
 
-    def __init__(self, in_nodes, out_nodes):
-        
+    def __init__(self, in_nodes, hid_nodes, hid_nodes2, out_nodes):
+        '''Initialize with random weights and biases, as well as layer sizes'''
         self.input_nodes = in_nodes
+        self.hidden_nodes = hid_nodes
+        self.hidden_nodes2 = hid_nodes2
         self.output_nodes = out_nodes
-        self.weights1 = 2* np.random.random((self.input_nodes, self.output_nodes)) -1
-        self.bias1 = 2* np.random.random((self.output_nodes)) -1
+        self.weights1 = 2* np.random.random((self.input_nodes, self.hidden_nodes)) -1
+        self.weights2 = 2* np.random.random((self.hidden_nodes,self.hidden_nodes2)) -1
+        self.weights3 = 2* np.random.random((self.hidden_nodes2,self.output_nodes)) -1
+        self.bias1 = 2* np.random.random((self.hidden_nodes)) -1
+        self.bias2 = 2* np.random.random((self.hidden_nodes2)) -1
+        self.bias3 = 2* np.random.random((self.output_nodes)) -1
 
-    # Activation functions
     def relu(self, x):
-        #using relu function
+        '''ReLU activation function returns positive number or 0'''
         return np.maximum(0,x)
     
     def feedForward(self, inputs):
-        inputs = np.asarray(inputs)  # convert inputs into np array
-        output = self.relu(np.dot(inputs, self.weights1)+ self.bias1)
+        '''Convert inputs to ouput percentages'''
+        inputs = np.asarray(inputs)
+        hidden = self.relu(np.dot(inputs, self.weights1)+ self.bias1) # 5 input neurons to 4 neurons
+        hidden2 = self.relu(np.dot(hidden, self.weights2)+ self.bias2) # 4 neurons to 4 neurons
+        output = self.relu(np.dot(hidden2, self.weights3)+ self.bias3) # 4 neurons to 3 neurons
         return output
         
-    def mutate(self, rate):   
+    def mutate(self, rate):
+        '''Mutate current weights by a certain degree'''
         def mutation (val):
+            # use a gaussian distribution to randomly change weights
             if (np.random.random(1) < rate):
                 rand = random.gauss(0, 0.1) + val
                 if (rand > 1):
                     rand = 1
                 elif (rand < -1):
                     rand =-1
-
                 return rand
             else:
                 return val
-        vmutate = np.vectorize(mutation)
+        vmutate = np.vectorize(mutation) # allows vmutate to be used like a numpy function
         self.weights1 = vmutate(self.weights1)
+        self.weights2 = vmutate(self.weights2)
+        self.weights3 = vmutate(self.weights3)
         self.bias1 = vmutate(self.bias1)
+        self.bias2 = vmutate(self.bias2)
+        self.bias3 = vmutate(self.bias3)
     
     def clone(self):
-        # Creates a clone of a neural network before overriding the random weights and biases
-        cloneBrain = NeuralNetwork(self.input_nodes, self.output_nodes)
+        '''creates a copy of the neural network before manually overriding the random weights from initialization with weights currently in use'''
+        cloneBrain = NeuralNetwork(self.input_nodes, self.hidden_nodes, self.hidden_nodes2, self.output_nodes)
         cloneBrain.weights1 = self.weights1
+        cloneBrain.weights2 = self.weights2
+        cloneBrain.weights3 = self.weights3
         cloneBrain.bias1 = self.bias1
+        cloneBrain.bias2 = self.bias2
+        cloneBrain.bias3 = self.bias3
+        
         return cloneBrain
     
